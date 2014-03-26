cc474bea334fcd170a247b9275ef209fefde0b34
Kyle Partridge <partkyle@gmail.com>
2014-03-26 11:23:33 -0700

    added usage support to the baseRef

--- envar.go
+++ envar.go
@@ -4,15 +4,21 @@ var references = make([]ref, 0)
 
 type ref interface {
 	Name() string
+	Usage() string
 	Set(string)
 }
 
 type basicRef struct {
-	name string
+	name  string
+	usage string
 }
 
 func (b *basicRef) Name() string {
 	return b.name
+}
+
+func (b *basicRef) Usage() string {
+	return b.usage
 }
 
 // Parses the config and loads all references using the
