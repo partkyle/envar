e0f71e35aa94d652bc0b8b1d15f2fcbd25e55846
Kyle Partridge <partkyle@gmail.com>
2014-03-25 17:48:58 -0700

    basic support for integers

--- envar.go
+++ envar.go
@@ -1,1 +1,54 @@
 package envar
+
+import (
+	"os"
+	"strconv"
+)
+
+var references = make([]ref, 0)
+
+type ref interface {
+	Name() string
+	SetValue(string) error
+}
+
+type intRef struct {
+	name string
+	def  int
+	ref  *int
+}
+
+func (i *intRef) Name() string {
+	return i.name
+}
+
+func (i *intRef) SetValue(env string) error {
+	val, err := strconv.Atoi(env)
+	if err != nil {
+		*i.ref = i.def
+		return err
+	}
+
+	*i.ref = val
+	return nil
+}
+
+func Int(name string, def int) *int {
+	ref := new(int)
+
+	iRef := &intRef{name: name, def: def, ref: ref}
+	references = append(references, iRef)
+
+	return ref
+}
+
+func Parse() error {
+	for _, ref := range references {
+		err := ref.SetValue(os.Getenv(ref.Name()))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
